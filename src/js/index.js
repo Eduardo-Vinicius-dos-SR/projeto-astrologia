8416c2e980584bc62984b1ae30cd8fb5f9a697fc
Eduardo-Vinicius-dos-SR <eduardovdsr@gmail.com>
2025-10-15 18:41:45 -0300

    Update geral

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,21 @@
-const buttonAgradecer = document.querySelector(".agradecimento button");
+const buttonAgradecer = document.querySelector(".btn-agradecimento button");
 const main = document.querySelector("main");
 const navegadores = document.querySelectorAll("nav ul li a");
+const agradecimento = document.querySelector(".agradecimento");
 
 navegadores.forEach((nav) => {
 	nav.addEventListener("click", () => {
 		main.classList.remove("agradecer");
+		agradecimento.classList.remove("agradecer");
 	});
 });
 
 buttonAgradecer.addEventListener("click", () => {
-	main.classList.add("agradecer");
-	alert("Funcionou!");
+	if (main.classList.contains("agradecer") || agradecimento.classList.contains("agradecer")) {
+		main.classList.remove("agradecer");
+		agradecimento.classList.remove("agradecer");
+	} else {
+		main.classList.add("agradecer");
+		agradecimento.classList.add("agradecer");
+	}
 });
